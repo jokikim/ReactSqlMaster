0a52c3d0494165a959d1e49f74b4da313f9c5409
JatinSingh08 <singhjatin0812@gmail.com>
2023-09-16 02:38:17 +0530

    ⚡Feat: Added reducers to appSlice

---
+++ src/components/editor/EditorTabs.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const EditorTabs = () => {
+  return (
+    <div className='flex flex-col w-full divide-y dark:divide-slate-700'>
+      <div
+        className={`${
+          fullScreen ? '' : 'hidden'
+        } flex justify-between dark:bg-slate-900 dark:text-slate-300`}
+      >
+        <div className='flex gap-x-1 max-w-[calc(100vw-350px)]'>
+          <div className='flex overflow-auto'>
+            {tabs.map((tab) => (
+              <div
+                key={tab.id}
+                className={`hover:border-b-blue-500 dark:hover:border-b-blue-400 mr-1 flex justify-center items-center whitespace-nowrap px-4 cursor-pointer ${
+                  activeTab === tab.id
+                    ? 'bg-white-100 border-b-2 border-b-blue-500'
+                    : 'bg-gray-200 dark:border-slate-800 border-b-2 dark:bg-slate-800 '
+                }`}
+                onClick={() => changeActiveTab(tab.id)}
+              >
+                <span> {tab.title}</span>
+
+                <button
+                  className='flex items-center justify-center ml-2 text-gray-400 hover:text-gray-900 dark:hover:text-slate-500'
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    removeTab(tab.id);
+                  }}
+                >
+                  <span className='-mr-1 material-symbols-outlined'>close</span>
+                </button>
+              </div>
+            ))}
+          </div>
+          <button
+            className='min-w-[50px] h-[50px] bg-gray-200 text-blue-600 dark:bg-slate-800 flex items-center justify-center hover:bg-gray-300 dark:hover:bg-slate-700 dark:text-blue-400'
+            onClick={() => addNewTab()}
+          >
+            <span className='material-symbols-outlined'>add</span>
+          </button>
+        </div>
+        {/* <DarkModeSwitch /> */}
+      </div>
+
+      {/* {tabs.map((tab) => (
+        <QueryPanel
+          key={tab.id}
+          tabId={tab.id}
+          initialQueryName={tab.queryName}
+          initialQuery={tab.query}
+        />
+      ))} */}
+    </div>
+  )
+}
+
+export default EditorTabs
--- src/redux/appSlice.js
+++ src/redux/appSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import throttle from "lodash.throttle";
 const initialState = {
   fullScreen: true,
   darkMode: true,
@@ -20,9 +20,83 @@ const appSlice = createSlice({
   name: 'app',
   initialState,
   reducers: {
-    toggleFullScreen: (state) => {
+    toggleFullScreen: throttle((state) => {
       state.fullScreen = !state.fullScreen;
-    }
+    }, 250),
+    toggleDarkMode: throttle((state) => {
+      state.darkMode = !state.darkMode;
+    }, 250),
+    saveNewQuery: (state, action) => {
+      const { queryName, query } = action.payload;
+      
+      if (!queryName) {
+        window.alert("Query Name Cannot be Empty");
+        return;
+      }
+      if (!query) {
+        window.alert("Query Cannot be Empty");
+        return;
+      }
+
+      const alreadyExists = queryName in state.savedQueries;
+      const newSavedQueries = { ...state.savedQueries, [queryName]: query };
+      localStorage.setItem("savedQueries", JSON.stringify(newSavedQueries));
+      window.alert(`${alreadyExists ? "Updated" : "Saved"} "${queryName}" Query!`);
+
+      state.savedQueries = newSavedQueries;
+    },
+    deleteSavedQuery: (state, action) => {
+      const queryName = action.payload;
+      const { [queryName]: _, ...remainingQueries } = state.savedQueries;
+      localStorage.setItem("savedQueries", JSON.stringify(remainingQueries));
+      state.savedQueries = remainingQueries;
+    },
+    increaseTabCount: (state) => {
+      state.tabCount += 1;
+    },
+    changeActiveTab: (state, action) => {
+      state.activeTab = action.payload;
+    },
+    addNewTab: (state, action) => {
+      const { queryName = "", query = "" } = action.payload;
+      if (state.tabs.length >= 15) {
+        window.alert("Max Tab Limit Reached!!!");
+        return;
+      }
+      const newTabId = state.tabCount + 1;
+      const newTabTitle = `Tab ${newTabId}`;
+      const newTabData = {
+        id: newTabId,
+        title: newTabTitle,
+        queryName: queryName,
+        query: query,
+      };
+      state.tabCount += 1;
+      state.activeTab = newTabId;
+      state.tabs.push(newTabData);
+    },
+    removeTab: (state, action) => {
+      const tabId = action.payload;
+      let newActiveTabId = null;
+      state.tabs = state.tabs.filter((tab) => {
+        if (tab.id !== tabId) {
+          return true;
+        } else {
+          const currentIndex = state.tabs.findIndex((tab) => tab.id === tabId);
+          const prevTab = state.tabs[currentIndex - 1];
+          const nextTab = state.tabs[currentIndex + 1];
+          newActiveTabId = prevTab ? prevTab.id : nextTab ? nextTab.id : null;
+          return false;
+        }
+      });
+      if (newActiveTabId === null) {
+        window.alert("There must be one active Tab!");
+        return;
+      }
+
+      state.activeTab = newActiveTabId;
+    },
+
   }
 })
 
